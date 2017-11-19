af81b20180ccd687fa9d613358c240e5c0f8ba8b
Gabriel Araújo <gabrielaraujo@Gabriels-MacBook-Air.local>
2017-11-19 13:47:21 -0200

    system calls

--- Interface/controle.c
+++ Interface/controle.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
   int pi, pj;
@@ -34,13 +35,13 @@ void atualiza(int ri) {
 
 void mostra(int ri) {
   fprintf(display, "%d %d %d %d %d\n",
-		  ri, rb[ri].pi, rb[ri].pj, rb[ri].i, rb[ri].j);
+      ri, rb[ri].pi, rb[ri].pj, rb[ri].i, rb[ri].j);
   atualiza(ri);
 }
 
 /* Programa simples para mostrar como controlar a arena */
 int main() {
-  int t; 						/* tempo */
+  int t;            /* tempo */
   display = popen("./apres", "r");
 
   rb[0].pi =  6;
@@ -54,20 +55,42 @@ int main() {
   rb[1].vj = -1;
 
   if (display == NULL) {
-  	fprintf(stderr,"Não encontrei o programa de exibição\n");
-  	return 1;
+    fprintf(stderr,"Não encontrei o programa de exibição\n");
+    return 1;
   }
   
-  /* cria dois robôs */
+  // Só para testes
+  // Determina onde ou não tem cristal
+  int i, j, n;
+  for (i = 0; i < 15; i++)
+    for (j = 0; j < 15; j++){
+      n = rand() % 100;
+      if (n < 80)
+        fprintf(display, "cristal 0 %d %d\n", i, j);  
+      else
+        if (n < 95)
+          fprintf(display, "cristal 1 %d %d\n", i, j);
+        else
+          fprintf(display, "cristal 2 %d %d\n", i, j);    
+    }
+    
+  // Determina onde são as bases
+  int x, y;
+  x = rand() % 15;
+  y = rand() % 15;
+  fprintf(display, "base BASE_A.png %d %d\n", x, y);
+  fprintf(display, "base BASE_B.png %d %d\n", y, x);
+
+  // Cria os dois robôs
   fprintf(display, "rob GILEAD_A.png\nrob GILEAD_B.png\n");
 
 
   for (t=0; t < 100; t++) {
-  	anda(0);
-  	anda(1);
-  	mostra(0);
-  	mostra(1);
-  	fflush(display);
+    anda(0);
+    anda(1);
+    mostra(0);
+    mostra(1);
+    fflush(display);
   }
 
   pclose(display);
